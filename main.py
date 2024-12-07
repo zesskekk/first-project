275cb65f1a914cc9f96bb845c50c0ab11517f76a
zesskekk <saiatbekasylai@gmail.com>
2024-12-07 19:00:29 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
-import random
-Asylai = [ '15 years old', 'high school student', 'dancer', 'loves physics and mathematics', 'likes reading']
-print(random.choice(Asylai))
+import telebot
+bot = telebot.TeleBot('8030316710:AAFp8_EHSl9V4OjAm15zoHgsrf1ru4TrlgE')
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text == "Привет":    
+        bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+    elif message.text == "/help":
+        bot.send_message(message.from_user.id, "Ты можешь написать "Привет", "Как дела?", "Хорошо", "Плохо".")
+    elif message.text == "ыыы":
+        bot.send_message(message.from_user.id, "не пиши мне непонятные символы")
+    elif message.text == "Как дела?":
+        bot.send_message(message.from_user.id, "Всё отлично! У тебя как?")
+    elif message.text == "Хорошо":
+        bot.send_message(message.from_user.id, "Хорошо, что хорошо:)")
+    elif message.text == "Плохо":
+        bot.send_message(message.from_user.id, "Жалко:(")   
+    else:
+        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+bot.polling(none_stop=True, interval=0)
